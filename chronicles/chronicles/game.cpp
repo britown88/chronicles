692e4c01d5ac4b150d8175f572356c10db85e44a
Brandon Townsend <p4r4digm@gmail.com>
2018-02-23 19:34:32 -0700

    reorganized game to use dialogs

--- chronicles/chronicles/game.cpp
+++ chronicles/chronicles/game.cpp
@@ -13,10 +13,28 @@ Game* gameCreate() {
 
 GameData* gameData(Game* game) { return &game->data; }
 
-static Texture* textTexture = nullptr;
-bool show_demo_window = true;
-bool show_another_window = false;
+static void _doStatsWindow(Window* wnd) {
 
+   ImGuiWindowFlags flags =
+      ImGuiWindowFlags_AlwaysAutoResize |
+      ImGuiWindowFlags_NoMove |
+      ImGuiWindowFlags_NoCollapse |
+      ImGuiWindowFlags_NoResize |
+      ImGuiWindowFlags_NoScrollbar |
+      ImGuiWindowFlags_NoTitleBar;
+
+   auto sz = windowSize(wnd);
+
+   ImGui::SetNextWindowPos(ImVec2(sz.x, ImGui::GetFrameHeightWithSpacing()), ImGuiCond_Always, ImVec2(1, 0));
+   if (ImGui::Begin("Stats", nullptr, flags)) {
+      auto txt = format("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      //auto txtSize = ImGui::CalcTextSize(txt.c_str());
+
+      //ImGui::SameLine(ImGui::GetWindowContentRegionWidth() - txtSize.x);
+      ImGui::Text(txt.c_str());
+   }
+   ImGui::End();
+}
 
 static void _mainMenu(Game* game, Window* wnd) {
    if (ImGui::BeginMainMenuBar()) {
@@ -47,6 +65,37 @@ static void _mainMenu(Game* game, Window* wnd) {
                return p_open;
             });
          }
+         if (ImGui::MenuItem("Goku")) {
+
+            Texture *gokuTex = nullptr;
+
+            windowAddGUI(wnd, "goku", [=](Window*wnd) mutable {
+               bool p_open = true;
+
+               if (!gokuTex) {
+                  gokuTex = textureCreateFromPath("goku.png", { RepeatType_CLAMP, FilterType_LINEAR });
+               }
+
+               if (ImGui::Begin("goku", &p_open)) {
+                  auto sz = ImGui::GetWindowContentRegionMax();
+                  ImGui::Image((ImTextureID)textureGetHandle(gokuTex), ImVec2(sz.x, sz.y));
+               }
+
+               if (!p_open) {
+                  textureDestroy(gokuTex);
+               }
+
+               ImGui::End();
+               return p_open;
+            });
+         }
+         if (ImGui::MenuItem("ImGui Demo")) {
+            windowAddGUI(wnd, "imguidemo", [=](Window*wnd) mutable {
+               bool p_open = true;
+               ImGui::ShowDemoWindow(&p_open);               
+               return p_open;
+            });
+         }
 
 
          ImGui::EndMenu();
@@ -59,52 +108,8 @@ static void _mainMenu(Game* game, Window* wnd) {
 void gameUpdate(Game* game, Window* wnd) {
    _mainMenu(game, wnd);
 
-   if (!textTexture) {
-      textTexture = textureCreateFromPath("goku.png", { RepeatType_CLAMP, FilterType_LINEAR });
-   }
+   _doStatsWindow(wnd);
 
-   if (ImGui::Begin("Test Texture")) {
-      auto sz = ImGui::GetWindowContentRegionMax();
-      ImGui::Image((ImTextureID)textureGetHandle(textTexture), ImVec2(sz.x, sz.y));
-   }
-   ImGui::End();
-
-   // 1. Show a simple window.
-   // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-   {
-      static float f = 0.0f;
-      static int counter = 0;
-      ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-      ImGui::ColorEdit3("clear color", (float*)&game->data.imgui.bgClearColor); // Edit 3 floats representing a color
-
-      ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-         counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-   }
-
-   // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-   if (show_another_window)
-   {
-      ImGui::Begin("Another Window", &show_another_window);
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me"))
-         show_another_window = false;
-      ImGui::End();
-   }
-
-   // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-   if (show_demo_window)
-   {
-      ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-      ImGui::ShowDemoWindow(&show_demo_window);
-   }
 }
 
 void gameDestroy(Game* game) {
