7d54c8e15e9ae8b1abcbd6209183edcf6f260278
Brandon Townsend <p4r4digm@gmail.com>
2018-02-24 23:58:01 -0700

    added nowide standalone

--- chronicles/chronicles/chronwin.cpp
+++ chronicles/chronicles/chronwin.cpp
@@ -1,9 +1,8 @@
 #include "chronwin.h"
+#include <nowide/convert.hpp>
 #include <Windows.h>
 
-
-
-
 std::string openFile(OpenFileConfig const& config) {
-
+   auto blah = nowide::widen(std::string("hello!"));
+   return 0;
 }
