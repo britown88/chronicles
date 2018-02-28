65a6ebc9dfd28cb8149a5a93ae7b2f283c2b33fd
Brandon Townsend <p4r4digm@gmail.com>
2018-02-27 20:52:25 -0700

    added the abilkity to zoom in and out of the ega encoder

--- chronicles/chronicles/uiEGAEncoder.cpp
+++ chronicles/chronicles/uiEGAEncoder.cpp
@@ -15,7 +15,8 @@ struct EncoderState {
    EGAPalette encPal;
    char palName[64];
 
-   
+   float zoomLevel = 1.0f;
+   Int2 zoomOffset = {};
 };
 
 static std::string _getPng() {
@@ -90,7 +91,8 @@ bool _doUI(Window* wnd, EncoderState &state) {
       if (ImGui::BeginChild("DrawArea", ImVec2(sz.x, sz.y - palHeight), true)) {
          if (state.pngTex) {
             auto csz = ImGui::GetContentRegionAvail();
-            auto rect = getProportionallyFitRect(textureGetSize(state.pngTex), { (i32)csz.x, (i32)csz.y });
+            auto texSize = textureGetSize(state.pngTex);
+            auto rect = Recti{ 0,0, texSize.x, texSize.y }; //getProportionallyFitRect(textureGetSize(state.pngTex), { (i32)csz.x, (i32)csz.y });
             rect.x = 0;
 
             ImDrawList* draw_list = ImGui::GetWindowDrawList();
@@ -100,16 +102,37 @@ bool _doUI(Window* wnd, EncoderState &state) {
                egaTextureDecode(state.ega, state.pngTex, &state.encPal);
             }
 
-            auto a = ImVec2(p.x + rect.x, p.y + rect.y);
-            auto b = ImVec2(p.x + rect.x + rect.w, p.y + rect.y + rect.h);
-
-            draw_list->AddRect(a, b, IM_COL32(0, 0, 0, 255));
-            //draw_list->AddRectFilled(a, b, IM_COL32_BLACK_TRANS);
-
-            ImGui::RenderColorRectWithAlphaCheckerboard(a, b, IM_COL32_BLACK_TRANS, 10, ImVec2());
-            draw_list->AddImage((ImTextureID)textureGetHandle(state.pngTex), a, b);
+            auto a = ImVec2(state.zoomOffset.x + p.x + rect.x, state.zoomOffset.y + p.y + rect.y);
+            auto b = ImVec2(state.zoomOffset.x + p.x + rect.x + rect.w*state.zoomLevel, state.zoomOffset.y + p.y + rect.y + rect.h*state.zoomLevel);
 
             
+            draw_list->PushClipRect(a, b, true);
+            draw_list->AddRect(a, b, IM_COL32(0, 0, 0, 255));
+            ImGui::RenderColorRectWithAlphaCheckerboard(a, b, IM_COL32_BLACK_TRANS, 10 * state.zoomLevel, ImVec2());
+            draw_list->PopClipRect();
+            //draw_list->AddRectFilled(a, b, IM_COL32_BLACK_TRANS);
+
+            
+            auto sz = ImVec2(rect.w*state.zoomLevel, rect.h*state.zoomLevel);
+            //ImGui::Image((ImTextureID)textureGetHandle(state.pngTex), sz);
+
+            ImGui::InvisibleButton("##dummy", csz);
+            if (ImGui::IsItemActive() && ImGui::IsMouseDragging()) { 
+               state.zoomOffset.x += ImGui::GetIO().MouseDelta.x; 
+               state.zoomOffset.y += ImGui::GetIO().MouseDelta.y;
+            }
+
+
+            draw_list->AddImage((ImTextureID)textureGetHandle(state.pngTex), a, b);
+
+            auto &io = ImGui::GetIO();
+            if (ImGui::IsWindowHovered(ImGuiHoveredFlags_ChildWindows)) { 
+
+               if (fabs(io.MouseWheel) > 0.0f) {
+                  state.zoomLevel = MIN(100, MAX(0.1f, state.zoomLevel + io.MouseWheel * 0.05f * state.zoomLevel));
+
+               }
+            }
          }
 
          ImGui::EndChild();
